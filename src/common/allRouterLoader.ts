4a0331027cbd4926df9eaa027230d11d30fd2f8e
cwanii66 <55499491+cwanii66@users.noreply.github.com>
2023-04-01 09:42:01 +0800

    feat: add router loader

---
+++ src/common/allRouterLoader.ts
@@ -0,0 +1,73 @@
+import path from 'node:path'
+import fg from 'fast-glob'
+import Router from 'koa-router'
+import body from 'koa-body'
+import json from 'koa-json'
+import type Koa from 'koa'
+
+// class for loading all router
+class RouterLoaderWrapper {
+  app!: Koa
+  static instance: RouterLoaderWrapper = new RouterLoaderWrapper()
+
+  // initialize routerloader
+  init(app: Koa) {
+    this.app = app
+    this.loadAllRouterWrapper()
+    process.nextTick(() => this.listen())
+  }
+
+  // load absolute path of all router files
+  getAbsoluteFilePaths() {
+    const dir = path.resolve(process.cwd(), 'src/router')
+    const routerFiles = fg.sync('*.ts', {
+      cwd: dir,
+      deep: 1,
+      absolute: true,
+    })
+    return routerFiles
+  }
+
+  // router type predicate
+  isRouter(pathData: unknown): pathData is Router {
+    return pathData instanceof Router
+  }
+
+  // load all 2 level routes to 1 level routes
+  loadAllRouterWrapper() {
+    const allAbsoluteFilePaths = this.getAbsoluteFilePaths()
+    const rootRouter = this.getRootRouter()
+    this.loadAllRouter(allAbsoluteFilePaths, rootRouter)
+    this.loadRootRouter(rootRouter)
+  }
+
+  // get 1 level(root) router
+  getRootRouter() {
+    const rootRouter = new Router()
+    rootRouter.prefix('/dang')
+    this.app.use(json())
+    this.app.use(body())
+    return rootRouter
+  }
+
+  loadRootRouter(rootRouter: Router) {
+    this.app.use(rootRouter.routes())
+  }
+
+  // load 2 level router
+  async loadAllRouter(allAbsoluteFilePaths: string[], rootRouter: Router) {
+    for (const routerPath of allAbsoluteFilePaths) {
+      const router = (await import(routerPath)).default
+      if (this.isRouter(router))
+        rootRouter.use(router.routes(), router.allowedMethods())
+    }
+  }
+
+  listen() {
+    this.app.listen(3002)
+    // eslint-disable-next-line no-console
+    console.log('server is running at port 3002')
+  }
+}
+
+export default RouterLoaderWrapper.instance
---
+++ src/router/user.ts
@@ -0,0 +1,26 @@
+import type { Context } from 'koa'
+import Router from 'koa-router'
+
+// type of userinfo
+interface UserInfo {
+  username: string
+  password: string
+  age: number
+}
+
+const router = new Router()
+
+router.prefix('/usermodule')
+
+router.get('/finduserinfo/:username', async (ctx: Context) => {
+  const { username } = ctx.params
+  ctx.body = `find user info by username: ${username}`
+})
+
+// post request -> add user
+router.post('/adduser', async (ctx: Context) => {
+  const { username, age }: UserInfo = ctx.request.body
+  ctx.body = `add user: ${username}, password: ${age}`
+})
+
+export default router
